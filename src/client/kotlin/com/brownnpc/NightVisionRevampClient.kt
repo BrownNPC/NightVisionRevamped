0e5d724eea52b11a142e78c69ae5f1eaf4b33e36
brownnpc <deaddogfuneral@gmail.com>
2025-05-18 17:04:50 +0500

    initial

---
+++ src/client/kotlin/com/brownnpc/NightVisionRevampClient.kt
@@ -0,0 +1,9 @@
+package com.brownnpc
+
+import net.fabricmc.api.ClientModInitializer
+
+object NightVisionRevampClient : ClientModInitializer {
+	override fun onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
