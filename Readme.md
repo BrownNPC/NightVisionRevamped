abc1dfa333c3e6732bf843c1a9baf5f0ba0116de
brownnpc <deaddogfuneral@gmail.com>
2025-05-22 20:32:43 +0500

    this took me 8 hours to figure out

---
+++ Readme.md
@@ -0,0 +1,15 @@
+# Night Vision Revamp
+
+This is a mod is a vanilla friendly alternative for people who use fullbright.
+
+It makes changes to the Glow Berries item, making them more useful for **cave diving**
+
+Glow berries will give 20 seconds of Night Vision.
+
+1x **Glow Ink Sac** *&*  1x **Glow Berries** will make a **Coated Glow Berry** that will give you:
+- slight Nausea (3 seconds)
+- Night Vision (2 minutes)
+- Glowing (20 seconds)
+
+I made this mod because my friends keep using fullbright. I recommend you use this 
+along with a dynamic lights mod so you can hold torches or lanterns in your offhand for light.
--- src/main/kotlin/com/brownnpc/NightVisionRevamp.kt
+++ src/main/kotlin/com/brownnpc/NightVisionRevamp.kt
@@ -1,15 +1,96 @@
 package com.brownnpc
 
 import net.fabricmc.api.ModInitializer
+import net.fabricmc.fabric.api.item.v1.DefaultItemComponentEvents
+import net.minecraft.component.DataComponentTypes
+import net.minecraft.component.type.ConsumableComponent
+import net.minecraft.component.type.FoodComponent
+import net.minecraft.entity.effect.StatusEffectInstance
+import net.minecraft.entity.effect.StatusEffects
+import net.minecraft.item.Item
+import net.minecraft.item.Items
+import net.minecraft.item.Item.Settings
+import net.minecraft.item.consume.ApplyEffectsConsumeEffect
+import net.minecraft.registry.Registries
+import net.minecraft.registry.Registry
+import net.minecraft.registry.RegistryKey
+import net.minecraft.registry.RegistryKeys
+import net.minecraft.util.Identifier
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents
+import net.minecraft.item.ItemGroups
 import org.slf4j.LoggerFactory
+import kotlin.random.Random
+
 
 object NightVisionRevamp : ModInitializer {
     private val logger = LoggerFactory.getLogger("night-vision-revamp")
 
-	override fun onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
-		logger.info("Hello Fabric world!")
-	}
+    override fun onInitialize() {
+    logger.info("Registering NightVisionRevamp...")
+    // Make coated glow berry item that is crafted with 1 glow ink sac and 1 glow berries
+    val id= Identifier.of("night-vision-revamp", "coated_glow_berries")
+    val key = RegistryKey.of(RegistryKeys.ITEM, id);
+    val settings = Item.Settings()
+        .registryKey(key).
+        food(
+            FoodComponent.Builder()
+            .alwaysEdible()
+            .build()
+        )
+    val COATED_GLOW_BERRY = Registry.register( // register custom item
+       Registries.ITEM,
+       key,
+       Item(settings)
+    )
+    // the custom item is a food
+    ItemGroupEvents.modifyEntriesEvent(ItemGroups.FOOD_AND_DRINK).register { group ->
+        group.add(COATED_GLOW_BERRY)
+    }
+
+    // it gives nausea for 7 seconds, makes you glow for 20, and gives you 2 mins of nightvision
+    DefaultItemComponentEvents.MODIFY.register( { ctx ->
+        ctx.modify(COATED_GLOW_BERRY) { builder ->
+            builder.add(
+                DataComponentTypes.CONSUMABLE,
+                ConsumableComponent.builder()
+                    .consumeEffect(
+                        ApplyEffectsConsumeEffect(
+                            listOf(
+                                StatusEffectInstance(StatusEffects.NAUSEA, 20 * 6), // 6 seconds (squid ink is gross)
+                                StatusEffectInstance(StatusEffects.GLOWING, 20 * 20), // 20 seconds (squid ink makes you glow)
+                                StatusEffectInstance(StatusEffects.NIGHT_VISION, 20 * 2*60) // 2 mins
+                            )
+                        )
+                    )
+                    .build()
+            )
+        }
+        // Modify vanilla glow berries to give 20 seconds of night vision
+        ctx.modify(Items.GLOW_BERRIES) { builder ->
+            builder.add(
+                DataComponentTypes.CONSUMABLE,
+                ConsumableComponent.builder()
+                    .consumeEffect(
+                        ApplyEffectsConsumeEffect(
+                            StatusEffectInstance(StatusEffects.NIGHT_VISION, 20 * 20)
+                        )
+                    )
+                    .build()
+            )
+            val food = builder.getOrDefault(DataComponentTypes.FOOD, FoodComponent.Builder().build())
+
+            builder.add(
+                DataComponentTypes.FOOD,
+                FoodComponent.Builder()
+                    .nutrition(food.nutrition())
+                    .saturationModifier(food.saturation())
+                    .alwaysEdible()
+                    .build()
+            )
+        }
+    })
+
+
+    }
 }
+
